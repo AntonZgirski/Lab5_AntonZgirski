2d665eb928498e7c926f2907fd2ae959c9b09066
AntonZgriski <anton.zgirski@gmail.com>
2022-12-27 00:15:18 +0300

    Lab5_Done

---
+++ Test_Semple/Communication.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_Semple
+{
+  public class Communication
+  {   
+    public List<Phone> phones = new List<Phone>();
+    public void Create(string name, string phone, double price, int size, double diag)
+    {
+      phones.Add(new Phone(name, phone, price, size, diag));
+    }
+    public string Read(int i)
+    {
+      return phones[i].ToString();
+    }
+    public void Update(double price, int i)
+    {
+      var phone = phones[i];
+      phone.Price = price;
+    }
+    public void Delete(int i)
+    {
+      phones.RemoveAt(i);
+    }
+
+    public string ReadAll()
+    {
+      StringBuilder st = new StringBuilder();
+      
+      foreach (var ph in phones)
+      {
+        st.AppendLine($"Brand Phone {ph.BrandName}. Model Phone {ph.ModelPhone}. Price {String.Format("{0:f2}", ph.Price)}. Memory size {ph.MemorySize}." +
+             $"Screen diagonal {ph.ScreenDiagonal}.");
+      }
+
+      return st.ToString();
+    }
+
+    public string PopularBrand()
+    {      
+      Dictionary<string, int> dicti = new Dictionary<string, int>();
+      dicti.Add(phones[0].BrandName, 1);
+      for (int i = 1; i < phones.Count; i++)
+      {        
+        if (dicti.ContainsKey(phones[i].BrandName))
+        {
+          dicti[phones[i].BrandName] += 1;
+        }
+        else
+        {
+          dicti.Add(phones[i].BrandName, 1);
+        }
+      }
+      
+      int max = 0;
+      string popBrandName = "";
+      foreach (var di in dicti)
+      {
+        if (di.Value > max)
+        {
+          popBrandName = di.Key;
+          max = di.Value;
+        }
+      }
+
+      return $"The most popular brand is {popBrandName}.";  
+    }
+  }
+
+  public class Phone
+  {
+    public string BrandName { get; set; }
+    public string ModelPhone { get; set; }
+    public double Price { get; set; }
+    public int MemorySize { get; set; }
+    public double ScreenDiagonal { get; set; }
+
+    public Phone(string name, string phone, double price, int size, double diag)
+    {
+      BrandName = name;
+      ModelPhone = phone;
+      Price = price;
+      MemorySize = size;
+      ScreenDiagonal = diag;
+    }
+
+    public override string ToString()
+    {
+      return $"Brand Phone {BrandName}. Model Phone {ModelPhone}. Price {String.Format("{0:f2}", Price)}. Memory size {MemorySize}." +
+             $"Screen diagonal {ScreenDiagonal}.";
+    }
+  }
+
+  public class WriteConsole
+  {
+    public void Print(string st)
+    {
+      Console.WriteLine(st);
+    }
+  }
+}
---
+++ Test_Semple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test_Semple
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var com = new Communication();
+      var console = new WriteConsole();
+      com.Create("Samsung", "S22 Ultra", 100.00, 128, 6.2);
+      com.Create("Samsung", "S21", 120.00, 256, 5.7);
+      com.Create("Apple", "Iphone 14", 500.00, 256, 6.7);
+      com.Create("Xiaomi", "Mi 12T", 400.00, 128, 7.0);
+      com.Create("Nokia", "3310", 20.00, 1, 3.4);
+      com.Create("Sony Ericson", "k750i", 1.00, 1, 1.5);
+      com.Create("Samsung", "S8", 120.00, 64, 5.8);
+
+      console.Print(com.ReadAll());
+      console.Print(com.Read(2));      
+
+      com.Update(150.0, 2);
+      com.Delete(3);
+
+      console.Print(com.ReadAll());
+      console.Print(com.PopularBrand());
+    }
+  }
+}
